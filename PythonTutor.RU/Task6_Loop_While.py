886fddc2f540dbe39cac3fd6255202ea664549be
TestQA14 <chemisova.irina2012@gmail.com>
2018-09-14 11:57:01 +0300

    add Tasks 6 -7

--- PythonTutor.RU/Task6_Loop_While.py
+++ PythonTutor.RU/Task6_Loop_While.py
@@ -272,4 +272,4 @@ while x_i != 0:
     partial_sum += x_i
     partial_sum_squares += x_i ** 2
     x_i = int(input())
-print(sqrt((partial_sum_squares - partial_sum ** 2 / n) / (n - 1)))
+print(math.sqrt((partial_sum_squares - partial_sum ** 2 / n) / (n - 1)))
